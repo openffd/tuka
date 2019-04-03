cb3a766437a55fcbec18dbf1f0bc7e09820b31dc
ffd <ffdelacruz@icloud.com>
2019-04-03 16:25:20 +0800

    Add update-library command

--- lib/tuka/cli.rb
+++ lib/tuka/cli.rb
@@ -2,24 +2,26 @@
 
 module Tuka
   class CLI < Thor
-    desc 'version', 'Show the Tuka version information'
-    def version
-      puts "v#{VERSION}"
-    end
-
     generate_library = Commands::GenerateLibrary
     generate_podfile = Commands::GeneratePodfile
+    update_library_desc = 'Updates a previously generated library from a Tukafile'
 
     register Commands::Init,        'init',             Commands::Init.usage,         Commands::Init.desc
+    register Commands::Automatic,   'automatic',        Commands::Automatic.usage,    Commands::Automatic.desc
     register generate_library,      'generate_library', generate_library.usage,       generate_library.desc
+    register generate_library,      'update_library',   'update-library',             update_library_desc
     register generate_podfile,      'generate_podfile', generate_podfile.usage,       generate_podfile.desc
     register Commands::AddReceptor, 'add_receptor',     Commands::AddReceptor.usage,  Commands::AddReceptor.desc
-    register Commands::Automatic,   'automatic',        Commands::Automatic.usage,    Commands::Automatic.desc
     register Commands::Uninstall,   'uninstall',        Commands::Uninstall.usage,    Commands::Uninstall.desc
 
     desc 'powerup!', "Let's power up!"
     def powerup!
       puts "\n(╯°□°）╯︵ ┻━┻"
+    end
+
+    desc 'version', 'Show the Tuka version information'
+    def version
+      puts "v#{VERSION}"
     end
 
     map %w[generate-lib update-lib gen-lib genlib gl ul] => :generate_library
