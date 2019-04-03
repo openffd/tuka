0c0e6c4bb2b9b707f0e92d17eaf834b6378022b5
ffd <ffdelacruz@icloud.com>
2019-04-03 15:50:56 +0800

    Pretty-format Tuka help display

--- lib/tuka/cli_help_patch.rb
+++ lib/tuka/cli_help_patch.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
 module Tuka
-  class CLI < Thor
+  class CLI
     class << self
       def help(shell, subcommand = false)
         list = printable_commands(true, subcommand)
         Thor::Util.thor_classes_in(self).each do |klass|
           list += klass.printable_commands(false)
         end
-        list.reject! { |list| list[0].split[1]  == 'help' }
+        list.reject! { |item| item[0].split[1]  == 'help' }.each do |item|
+          item[0].gsub! 'tuka ', ''
+          item[1].gsub! '# ', ' '
+        end
 
         shell.say "\nJust another automation tool for the iOS developers of YJN.", :magenta
-        shell.say "\nCommands:", :cyan
+        shell.say "\nUsage:", :yellow
+        shell.say '  tuka [OPTIONS] [COMMAND]'
+        shell.say "\nCommands:", :yellow
 
         shell.print_table(list, :indent => 2, :truncate => true)
-        shell.say
-        class_options_help(shell)
-
-        shell.say "Run 'tuka -h [COMMAND]' for more information on a command.", :cyan
+        shell.say "\nRun 'tuka -h [COMMAND]' for more information on a command.", :yellow
       end
     end
   end
