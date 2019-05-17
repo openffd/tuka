7766ec6a9d00f0aa8b72090d96e02f1c2dd1059b
ffd <ffdelacruz@icloud.com>
2019-05-17 11:35:10 +0800

    Rename #has_file_reference? for Rubocop convention

--- lib/tuka/xcodeproj/overrides.rb
+++ lib/tuka/xcodeproj/overrides.rb
@@ -2,12 +2,12 @@
 
 module Xcodeproj
   Project::Object::PBXGroup.class_eval do
-    def has_file_reference?(file_reference)
+    def file_reference?(file_reference)
       files.map(&:path).include? file_reference
     end
 
     def add_new_file(path, source_tree = :group)
-      new_file(path, source_tree) unless has_file_reference?(path)
+      new_file(path, source_tree) unless file_reference?(path)
     end
   end
 end
