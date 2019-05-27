251cef43f49957341ab1e0bcf5a3889421229473
ffd <ffdelacruz@icloud.com>
2019-05-27 15:33:08 +0800

    Add update receptor command

--- lib/tuka/cli.rb
+++ lib/tuka/cli.rb
@@ -4,16 +4,24 @@ module Tuka
   class CLI < Thor
     generate_library = Commands::GenerateLibrary
     generate_podfile = Commands::GeneratePodfile
-    update_library_desc = 'Updates a previously generated library according to new Tukafile configuration'
-    update_podfile_desc = 'Updates a previously generated podfile according to new Tukafile configuration'
+
+    update_library_usage  = 'update-library'
+    update_library_desc   = 'Updates previously generated library according to new Tukafile configuration'
+
+    update_podfile_usage  = 'update-podfile'
+    update_podfile_desc   = 'Updates previously generated podfile according to new Tukafile configuration'
+
+    update_receptor_usage = 'update-receptor'
+    update_receptor_desc  = 'Updates previously generated receptor files according to new Tukafile configuration'
 
     register Commands::Init,        'init',             Commands::Init::USAGE,          Commands::Init.desc
     register Commands::Automatic,   'automatic',        Commands::Automatic::USAGE,     Commands::Automatic.desc
     register generate_library,      'generate_library', generate_library::USAGE,        generate_library.desc
-    register generate_library,      'update_library',   'update-library',               update_library_desc
+    register generate_library,      'update_library',   update_library_usage,           update_library_desc
     register generate_podfile,      'generate_podfile', generate_podfile::USAGE,        generate_podfile.desc
-    register generate_podfile,      'update_podfile',   'update-podfile',               update_podfile_desc
+    register generate_podfile,      'update_podfile',   update_podfile_usage,           update_podfile_desc
     register Commands::AddReceptor, 'add_receptor',     Commands::AddReceptor::USAGE,   Commands::AddReceptor.desc
+    register Commands::AddReceptor, 'update_receptor',  update_receptor_usage,          update_receptor_desc
     register Commands::Uninstall,   'uninstall',        Commands::Uninstall::USAGE,     Commands::Uninstall.desc
 
     desc 'powerup!', "Let's power up!"
@@ -28,7 +36,7 @@ module Tuka
 
     map %w[generate-lib update-lib gen-lib genlib gl ul] => :generate_library
     map %w[generate-pod update-pod gen-pod genpod gp up] => :generate_podfile
-    map %w[add-recep add-r addr ar] => :add_receptor
+    map %w[add-recep update-receptor add-r addr updr ar ur] => :add_receptor
     map %w[install auto au a] => :automatic
     map %w[rm u] => :uninstall
     map %w[pow] => :powerup!
