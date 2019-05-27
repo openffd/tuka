1d3b60e02e3f5569b2788ffc173e80bd9fbcb340
ffd <ffdelacruz@icloud.com>
2019-05-27 12:17:34 +0800

    WIP: Curl with authentication implementation for nextcloud option

--- lib/tuka/commands/init.rb
+++ lib/tuka/commands/init.rb
@@ -16,7 +16,6 @@ module Tuka
       # class_option :local,      aliases: ['-l'], desc: 'Copy a Tukafile from the local file system'
 
       def check_class_options
-        @specific_options = [options[:curl], options[:git], options[:local]].compact
         raise StandardError, 'Invalid combination of options for this command' if specific_options.count > 1
       end
 
@@ -26,11 +25,19 @@ module Tuka
 
       def download_tukafile
         puts "\nSourcing Tukafile from #{url}"
-        if @specific_options.empty? || @specific_options[:curl]
+        if specific_options.empty? || options[:curl]
           # curl = Curl::Easy.new(url)
-        elsif @specific_options[:git]
+        elsif options[:git]
           raise StandardError, 'Failed to download from given repository URL' unless git_clone(url, TukaBundle.dir)
-        elsif @specific_options[:local]
+        elsif options[:nextcloud]
+          curl = Curl::Easy.new(url)
+          curl.http_auth_types = :basic
+          curl.username = 'username'
+          curl.password = 'password123!'
+          curl.on_complete  { |curl| puts 'Complete' }
+          curl.on_success   { |curl| puts 'Success' }
+          curl.perform
+        elsif options[:local]
           # TODO: Implement sourcing Tukafile from local file system
         end
       end
@@ -67,6 +74,12 @@ module Tuka
 
       def display_command_completion
         puts "\nEnd"
+      end
+
+      private
+
+      def specific_options
+        [options[:curl], options[:git], options[:local], options[:nextcloud]].compact
       end
     end
   end
