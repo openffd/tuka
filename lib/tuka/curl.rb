60aebc36c34be06d4712e8ea2e8d58f98ff990be
ffd <ffdelacruz@icloud.com>
2019-05-28 14:48:37 +0800

    Return response code for Shortcut module methods; Add conditional for displaying curl status messages

--- lib/tuka/curl.rb
+++ lib/tuka/curl.rb
@@ -1,32 +1,36 @@
 # frozen_string_literal: true
 
 module Curl
+  class Easy
+    attr_accessor :should_display_msg
+  end
+
   def self.basic_curl
     curl = Curl::Easy.new
     curl.connect_timeout = 4
     curl.timeout = 4
-    curl.on_complete  { |curl| puts '[✓] Curl completed with status: ' + curl.status }
-    curl.on_success   { |_| puts '[✓] Curl successful' }
-    curl.on_failure   { |_, code| puts 'Curl Error: ' + code }
+    curl.should_display_msg = false
+    curl.on_complete  {|curl| puts '[✓] Curl completed with status: ' + curl.status.yellow if should_display_msg }
+    curl.on_success   {|curl| puts '[✓] Curl was successful' if should_display_msg }
+    curl.on_failure   {|_, code| puts 'Curl Error: ' + code if should_display_msg }
     curl
   end
 
   module Shortcuts
-    def curl(url)
+    def curl_instance(url:)
       curl = Curl.basic_curl.dup
       curl.url = url
       curl.perform
-      curl
+      curl.response_code
     end
 
-    def curl_with_auth(url)
+    def curl_instance_with_auth(url:)
       curl = Curl.basic_curl.dup
       curl.url              = url
       curl.http_auth_types  = :basic
       curl.username         = 'username'
       curl.password         = 'password123!'
-      curl.perform
-      curl
+      curl.response_code
     end
   end
 end
