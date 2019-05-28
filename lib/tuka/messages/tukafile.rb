0a66c080b4187e83700f02747870ba05c78d38a8
ffd <ffdelacruz@icloud.com>
2019-05-28 23:30:50 +0800

    Complete all Tukafile error messages

--- lib/tuka/messages/tukafile.rb
+++ lib/tuka/messages/tukafile.rb
@@ -2,20 +2,23 @@
 
 module Tuka
   module Messages
-    module Tukafile
-      INVALID_LIBRARY                     = 'IL'
-      INVALID_PROJECT_INFO_BUNDLE_ID      = 'IPIBI'
-      INVALID_PROJECT_INFO_HEADERS        = 'IPIH'
-      INVALID_PROJECT_INFO_RECEPTOR_NAME  = 'IPIRN'
-      INVALID_PROJECT_INFO_SWIFT_VERSION  = '123123'
-      INVALID_PROJECT_INFO_TYPE           = '12312'
-      INVALID_PROJECT_INFO_XCODEPROJ      = '354'
-      INVALID_SERVER_INACTIVE_DAYS        = '5425'
-      INVALID_SERVER_PROTOCOL             = '12312'
-      INVALID_SERVER_URL                  = 'w3eeq2'
-      INVALID_SERVER_URL_PATH             = 'asddasasd'
-      INVALID_SERVER_URL_TYPE             = 'asdasd'
-      INVALID_SERVER_USER_AGENT           = 'asd'
+    module TukafileErrors
+      filename    = Tukafile::BASENAME
+      days_range  = Tukafile::DAYS_RANGE
+
+      INVALID_LIBRARY                     = filename + " `library.digest' was tampered"
+      INVALID_PROJECT_INFO_BUNDLE_ID      = filename + " `project_info.bundle_id' is invalid"
+      INVALID_PROJECT_INFO_HEADERS        = filename + " `project_info.headers' is invalid"
+      INVALID_PROJECT_INFO_RECEPTOR_NAME  = filename + " `project_info.receptor_name' is invalid"
+      INVALID_PROJECT_INFO_SWIFT_VERSION  = filename + " `project_info.swift_version' is invalid"
+      INVALID_PROJECT_INFO_TYPE           = filename + " `project_info.type' is invalid"
+      INVALID_PROJECT_INFO_XCODEPROJ      = filename + " `project_info.xcodeproj' is invalid"
+      INVALID_SERVER_INACTIVE_DAYS        = filename + " `server.inactive_days' range: (#{days_range})"
+      INVALID_SERVER_PROTOCOL             = filename + " `server.protocol' is invalid"
+      INVALID_SERVER_URL                  = filename + " `server.url' is invalid"
+      INVALID_SERVER_URL_PATH             = filename + " `server.url_path' is invalid"
+      INVALID_SERVER_URL_TYPE             = filename + " `server.url_type' is invalid"
+      INVALID_SERVER_USER_AGENT           = filename + " `server.user_agent' is invalid"
     end
   end
 end
