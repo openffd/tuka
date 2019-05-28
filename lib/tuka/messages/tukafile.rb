4b7c9fcf25261cea9297c056aee2df9bfb4eb12a
ffd <ffdelacruz@icloud.com>
2019-05-28 18:59:52 +0800

    Add Messages module; Add Tukafile messages module within Messages

---
+++ lib/tuka/messages/tukafile.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Tuka
+  module Messages
+    module Tukafile
+      INVALID_LIBRARY                     = 'IL'
+      INVALID_PROJECT_INFO_BUNDLE_ID      = 'IPIBI'
+      INVALID_PROJECT_INFO_HEADERS        = 'IPIH'
+      INVALID_PROJECT_INFO_RECEPTOR_NAME  = 'IPIRN'
+      INVALID_PROJECT_INFO_SWIFT_VERSION  = '123123'
+      INVALID_PROJECT_INFO_TYPE           = '12312'
+      INVALID_PROJECT_INFO_XCODEPROJ      = '354'
+      INVALID_SERVER_INACTIVE_DAYS        = '5425'
+      INVALID_SERVER_PROTOCOL             = '12312'
+      INVALID_SERVER_URL                  = 'w3eeq2'
+      INVALID_SERVER_URL_PATH             = 'asddasasd'
+      INVALID_SERVER_URL_TYPE             = 'asdasd'
+      INVALID_SERVER_USER_AGENT           = 'asd'
+    end
+  end
+end
