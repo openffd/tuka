528e61d6e08b0a1f08a05dbfcecf398c8a349e00
ffd <ffdelacruz@icloud.com>
2019-05-29 16:45:21 +0800

    Create cmd_list method in CLI eigenklass

--- lib/tuka/cli_patch.rb
+++ lib/tuka/cli_patch.rb
@@ -4,6 +4,16 @@ module Tuka
   class CLI
     class << self
       def help(shell, subcommand = false)
+        shell.say "\nYet another automation tool for YJN iOS Devs.", :magenta
+        shell.say "\nUsage:", :yellow
+        shell.say '  tuka [COMMAND] [ARGS] [OPTIONS]'
+        shell.say "\nCommands:", :yellow
+        shell.print_table(cmd_list(subcommand), indent: 2, truncate: true)
+        shell.say "\nHelp:", :yellow
+        shell.say "  Run 'tuka -h [COMMAND]' for more information on a command."
+      end
+
+      def cmd_list(subcommand)
         list = printable_commands(true, subcommand)
         Thor::Util.thor_classes_in(self).each do |klass|
           list += klass.printable_commands(false)
@@ -12,15 +22,6 @@ module Tuka
           item[0].gsub! 'tuka ', ''
           item[1].gsub! '# ', '  '
         end
-
-        shell.say "\nYet another automation tool for YJN iOS Devs.", :magenta
-        shell.say "\nUsage:", :yellow
-        shell.say '  tuka [OPTIONS] [COMMAND]'
-        shell.say "\nCommands:", :yellow
-
-        shell.print_table(list, indent: 2, truncate: true)
-        shell.say "\nHelp:", :yellow
-        shell.say "  Run 'tuka -h [COMMAND]' for more information on a command."
       end
     end
   end
