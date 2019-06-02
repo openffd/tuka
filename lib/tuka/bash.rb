280c848da917bb5596829ab363b82f55dfd0000a
ffd <ffdelacruz@icloud.com>
2019-06-02 19:52:49 +0800

    Module Bash#tukarc

--- lib/tuka/bash.rb
+++ lib/tuka/bash.rb
@@ -4,6 +4,7 @@ module Tuka
   module Bash
     BASH_PROFILE_PATH = '~/.bash_profile'
     BASHRC_PATH       = '~/.bashrc'
+    TUKARC_PATH       = '~/.tukarc'
 
     def bash_profile
       begin File.open(File.expand_path(BASH_PROFILE_PATH)) rescue nil end
@@ -11,6 +12,10 @@ module Tuka
 
     def bashrc
       begin File.open(File.expand_path(BASHRC_PATH)) rescue nil end
+    end
+
+    def tukarc
+      begin File.open(File.expand_path(TUKARC_PATH)) rescue nil end
     end
   end
 end
