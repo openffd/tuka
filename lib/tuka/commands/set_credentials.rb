7381b0ec5f889fd7982967dd6fbfc4ecfe901800
ffd <ffdelacruz@icloud.com>
2019-06-03 21:40:41 +0800

    WIP: SetCredentials #check_prior_setup and #check_bash_profile

--- lib/tuka/commands/set_credentials.rb
+++ lib/tuka/commands/set_credentials.rb
@@ -13,17 +13,28 @@ module Tuka
       end
 
       def check_prior_setup
-        # if tukarc && bash_profile &&
-        puts 'Looks like credentials have been setup already.' unless tukarc.nil? || bash_profile.nil?
+        lambda {
+          print_newline
+          puts "The required credentials are already set. There's no need for a setup. Exiting..."
+          exit
+        }.call if tukarc_setup?
+      rescue
+      end
+
+      def check_bash_profile
+        print_newline
+        puts '[✓] Checked `~/.bash_profile`: OK' if bash_profile_setup?
+      rescue BashProfileTukarcNotSourcedError
+        puts '[✓] Detected `~/.bash_profile`; Added lines to source the `~/.tukarc` file.'
+        add_line_sourcing_tukarc_to_bash_profile
+      rescue BashProfileMissingError
+        puts '[✓] Created `~/.bash_profile`'
+        create_bash_profile
       end
 
       def create_tukarc
         puts 'Unable to locate a `.tukarc` file in the home directory. Creating one...' if tukarc.nil?
       end
-
-      # def check_bash_profile_existence
-      #   if bash_profile.nil?
-      # end
     end
   end
 end
