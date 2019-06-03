43821aefe5df761d0adf2e65afef29ce72cd6483
ffd <ffdelacruz@icloud.com>
2019-06-03 20:26:45 +0800

    Add convenience methods for setting up bash_profile to Bash module

--- lib/tuka/bash.rb
+++ lib/tuka/bash.rb
@@ -2,6 +2,8 @@
 
 module Tuka
   module Bash
+    using CoreExtensions
+
     BASH_PROFILE_PATH = '~/.bash_profile'
     BASHRC_PATH       = '~/.bashrc'
     TUKARC_PATH       = '~/.tukarc'
@@ -11,6 +13,12 @@ module Tuka
 
     ENV_BITBUCKET_USERNAME = ENV['BITBUCKET_USERNAME']
     ENV_BITBUCKET_PASSWORD = ENV['BITBUCKET_PASSWORD']
+
+    def bashrc
+      File.open(File.expand_path(BASHRC_PATH))
+    rescue StandardError
+      nil
+    end
 
     class BashProfileMissingError < StandardError
     end
@@ -31,14 +39,16 @@ module Tuka
       true
     end
 
-    def source_bash_profile
-      system '.', BASH_PROFILE_PATH
+    def create_bash_profile
+      touch(filename: File.expand_path(BASH_PROFILE_PATH), content: LOAD_TUKARC_CMD)
     end
 
-    def bashrc
-      File.open(File.expand_path(BASHRC_PATH))
-    rescue StandardError
-      nil
+    def add_line_sourcing_tukarc_to_bash_profile
+      append_to_file(File.expand_path(BASH_PROFILE_PATH), string: "\n#{LOAD_TUKARC_CMD}")
+    end
+
+    def source_bash_profile
+      system '.', BASH_PROFILE_PATH
     end
   end
 end
