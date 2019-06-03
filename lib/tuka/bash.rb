1e54a5a1b50cceca133c3279bf7207e1f111357c
ffd <ffdelacruz@icloud.com>
2019-06-03 12:36:33 +0800

    Add more methods and constants as needed for Bash module

--- lib/tuka/bash.rb
+++ lib/tuka/bash.rb
@@ -6,8 +6,24 @@ module Tuka
     BASHRC_PATH       = '~/.bashrc'
     TUKARC_PATH       = '~/.tukarc'
 
+    LOAD_TUKARC_CMD       = "[ -r #{TUKARC_PATH} ] && . #{TUKARC_PATH}"
+    LOAD_TUKARC_CMD_REGEX = /\n\[ -r ~\/.tukarc \] && . ~\/.tukarc\n/
+
+    ENV_BITBUCKET_USERNAME = ENV['BITBUCKET_USERNAME']
+    ENV_BITBUCKET_PASSWORD = ENV['BITBUCKET_PASSWORD']
+
     def bash_profile
       begin File.open(File.expand_path(BASH_PROFILE_PATH)) rescue nil end
+    end
+
+    def bash_profile_setup?
+      return false if bash_profile.nil?
+
+      bash_profile.read =~ LOAD_TUKARC_CMD_REGEX
+    end
+
+    def source_bash_profile
+      system '.', BASH_PROFILE_PATH
     end
 
     def bashrc
@@ -16,6 +32,32 @@ module Tuka
 
     def tukarc
       begin File.open(File.expand_path(TUKARC_PATH)) rescue nil end
+    end
+
+    class TukarcMissingError < StandardError
+      def initialize(msg = 'Unable to locate a `.tukarc` file in the home directory.')
+        super
+      end
+    end
+
+    class TukarcNotLoadedError < StandardError
+      def initialize(msg = 'The `.tukarc` file in the home directory is not sourced.')
+        super
+      end
+    end
+
+    class TukarcIncompleteVarsError < StandardError
+      def initialize(msg = '')
+        super
+      end
+    end
+
+    def tukarc_setup?
+      raise TukarcMissingError if tukarc.nil?
+      raise TukarcNotLoadedError if ENV_BITBUCKET_USERNAME.nil? && ENV_BITBUCKET_PASSWORD.nil
+      raise TukarcIncompleteVarsError if ENV_BITBUCKET_USERNAME.to_s.strip.empty?
+      raise TukarcIncompleteVarsError if ENV_BITBUCKET_PASSWORD.to_s.strip.empty?
+      true
     end
   end
 end
