83821b8738f0817e06896a2e3647bf2f0b6e3d4b
ffd <ffdelacruz@icloud.com>
2019-06-22 17:06:13 +0800

    Create invalid prefix message for Tukafile

--- lib/tuka/messages/tukafile_errors.rb
+++ lib/tuka/messages/tukafile_errors.rb
@@ -9,6 +9,7 @@ module Tuka
       INVALID_LIBRARY                     = filename + " `library.digest' was tampered"
       INVALID_PROJECT_INFO_BUNDLE_ID      = filename + " `project_info.bundle_id' is invalid"
       INVALID_PROJECT_INFO_HEADERS        = filename + " `project_info.headers' is invalid"
+      INVALID_PROJECT_INFO_PREFIX         = filename + " `project_info.prefix' is invalid"
       INVALID_PROJECT_INFO_RECEPTOR_NAME  = filename + " `project_info.receptor_name' is invalid"
       INVALID_PROJECT_INFO_SWIFT_VERSION  = filename + " `project_info.swift_version' is invalid"
       INVALID_PROJECT_INFO_TYPE           = filename + " `project_info.type' is invalid"
