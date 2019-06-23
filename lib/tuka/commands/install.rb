351f891af2f8f934eaaa973bd24f76c17d970b40
ffd <ffdelacruz@icloud.com>
2019-06-23 17:25:14 +0800

    Awesome refactor for Install command

--- lib/tuka/commands/install.rb
+++ lib/tuka/commands/install.rb
@@ -3,24 +3,12 @@
 module Tuka
   module Commands
     class Install < Command
-      using CoreExtensions
-
-      def generate_library
-        cmd = GenerateLibrary.new
-        cmd.options = { quiet: true }
-        cmd.invoke_all
-      end
-
-      def generate_podfile
-        cmd = GeneratePodfile.new
-        cmd.options = { quiet: true, yes: true }
-        cmd.invoke_all
-      end
-
-      def generate_receptor
-        cmd = GenerateReceptor.new
-        cmd.options = { quiet: true }
-        cmd.invoke_all
+      def install
+        [GenerateLibrary, GeneratePodfile, GenerateReceptor].each do |klass|
+          cmd = klass.new
+          cmd.options = { quiet: true, yes: true }
+          cmd.invoke_all
+        end
       end
 
       def display_command_completion
