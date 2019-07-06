ad9c353f387ef1f1993f2f63a39a073dc420f6d7
ffd <ffdelacruz@icloud.com>
2019-07-06 10:49:41 +0800

    Project#configurator for better context

--- lib/tuka/project_bundle/project.rb
+++ lib/tuka/project_bundle/project.rb
@@ -9,7 +9,7 @@ module Tuka
     using CoreExtensions::StringPrefix
 
     attr_reader :name, :category_prefix
-    attr_accessor :project_configurator
+    attr_accessor :configurator
 
     TYPES = { ObjC: 'objc', Swift: 'swift', Unity: 'unity' }.freeze
     TYPES.values.each do |type|
@@ -20,7 +20,7 @@ module Tuka
 
     def initialize(xcodeproj)
       @xcodeproj = xcodeproj
-      @project_configurator = Xcodeproj::Project.open(xcodeproj)
+      @configurator = Xcodeproj::Project.open(xcodeproj)
       @name = File.basename(@xcodeproj, '.*')
       @category_prefix = name.remove_non_letter_chars.generate_prefix.upcase
       @pbxproj_path = File.join(@xcodeproj, PBXProj::BASENAME)
@@ -44,17 +44,17 @@ module Tuka
 
     def new_file_destination_group
       type_key = TYPES.key(type)
-      reference = @project_configurator.files.select { |file| file.path =~ /#{type_search_strings[type_key]}/ }.first
+      reference = @configurator.files.select { |file| file.path =~ /#{type_search_strings[type_key]}/ }.first
       return nil if reference.nil?
 
       reference.parent
     end
 
     def always_embed_swift_standard_libraries
-      @project_configurator.targets.first.build_configurations.each do |configuration|
+      @configurator.targets.first.build_configurations.each do |configuration|
         configuration.build_settings[ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES] = INHERITED_FLAG
       end
-      @project_configurator.save
+      @configurator.save
     end
 
     # def receptor_files
@@ -62,7 +62,7 @@ module Tuka
     # end
 
     def delete_previous_receptor_files
-      file_references = @project_configurator.files.select { |file| file.path =~ /AppDelegate\+/ }
+      file_references = @configurator.files.select { |file| file.path =~ /AppDelegate\+/ }
       return if file_references.empty?
 
       groups_for_deletion(file_references).flatten.each do |ref|
@@ -70,27 +70,27 @@ module Tuka
         ref.remove_from_project
       end
 
-      @project_configurator.save
+      @configurator.save
     end
 
     def add_new_receptor_files(h_file:, m_file:)
       new_file_destination_group.add_new_file(File.basename(h_file))
       m_file_ref = new_file_destination_group.add_new_file(File.basename(m_file))
 
-      @project_configurator.targets.first.add_file_references([m_file_ref]) if m_file_ref
-      @project_configurator.save
+      @configurator.targets.first.add_file_references([m_file_ref]) if m_file_ref
+      @configurator.save
     end
 
     def bridging_header
-      @project_configurator.targets.first.build_configurations.first.build_settings[SWIFT_OBJC_BRIDGING_HEADER]
+      @configurator.targets.first.build_configurations.first.build_settings[SWIFT_OBJC_BRIDGING_HEADER]
     end
 
     def register_bridging_header(bridging_header)
       reference = new_file_destination_group.new_file(File.basename(bridging_header))
-      @project_configurator.targets.first.build_configurations.each do |config|
+      @configurator.targets.first.build_configurations.each do |config|
         config.build_settings[SWIFT_OBJC_BRIDGING_HEADER] = reference.full_path.to_s
       end
-      @project_configurator.save
+      @configurator.save
     end
 
     def info_plist_path
@@ -102,12 +102,12 @@ module Tuka
     end
 
     def contains_user_notifications_framework?
-      references = @project_configurator.targets.first.frameworks_build_phase.files_references
+      references = @configurator.targets.first.frameworks_build_phase.files_references
       references.any? { |ref| ref.name == Frameworks.user_notifications_framework }
     end
 
     def add_user_notifications_framework
-      frameworks_group = @project_configurator.groups.select { |group| group.name == Frameworks.group_name }.first
+      frameworks_group = @configurator.groups.select { |group| group.name == Frameworks.group_name }.first
       return if frameworks_group.nil?
 
       add_user_notification_framework_to_group(frameworks_group)
@@ -129,7 +129,7 @@ module Tuka
 
     def possible_app_delegate_files
       searchables = PBXProj.constants.grep(/SEARCHABLE_*/).map(&PBXProj.method(:const_get))
-      @project_configurator.files.select do |file|
+      @configurator.files.select do |file|
         searchables.include? File.basename(file.real_path)
       end
     end
@@ -167,8 +167,8 @@ module Tuka
     def add_user_notification_framework_to_group(group)
       reference = group.new_file(Frameworks.user_notifications_framework_path)
       reference.set_source_tree(Frameworks.source_tree)
-      @project_configurator.targets.first.frameworks_build_phase.add_file_reference(reference)
-      @project_configurator.save
+      @configurator.targets.first.frameworks_build_phase.add_file_reference(reference)
+      @configurator.save
     end
   end
 end
