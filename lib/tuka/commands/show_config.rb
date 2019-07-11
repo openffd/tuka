a16bf1583b08b663ffa3ecf433aa4c647513ad11
ffd <ffdelacruz@icloud.com>
2019-07-11 18:02:23 +0800

    Complete the ShowConfig command functionality

--- lib/tuka/commands/show_config.rb
+++ lib/tuka/commands/show_config.rb
@@ -2,15 +2,22 @@
 
 module Tuka
   module Commands
+    using CoreExtensions
+
     class ShowConfig < Command
       def display_show_config
         puts
-        puts "Locating Tuka config file for #{project.name} (#{project.type_pretty})".magenta
+        puts "Locating Tuka config file for #{project.name} (#{project.type_pretty})...".magenta
+        puts
+      end
+
+      def check_tukafile
+        raise StandardError, 'Tukafile not found' unless tuka_bundle_dir && File.file?(generated_tukafile_path)
       end
 
       def open_config
-        path = tukafile.path
-        xed(path) if path
+        puts 'Opening Tukafile...'
+        xed(tukafile.path)
       end
     end
   end
