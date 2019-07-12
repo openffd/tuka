dba34677df1c27cbe1d35172977b978b80add85c
ffd <ffdelacruz@icloud.com>
2019-07-12 13:14:01 +0800

    The extra move command in git_clone is no longer necessary because the cargo_dir is no longer a dot folder

--- lib/tuka/git.rb
+++ lib/tuka/git.rb
@@ -4,15 +4,9 @@ module Tuka
   module Git
     GITIGNORE_BASENAME = '.gitignore'
 
-    def git_clone(url, dir = Dir.pwd, cargo_dir = nil)
-      extra_mv_cmd = lambda {
-        return nil if cargo_dir.nil?
-
-        "#{dir}/tmp/#{cargo_dir}"
-      }.call
-
+    def git_clone(url, dir = Dir.pwd)
       rm_tmp_cmd = "rm -rf #{dir}/tmp"
-      system "#{rm_tmp_cmd}; git clone #{url} #{dir}/tmp && mv #{dir}/tmp/* #{extra_mv_cmd} #{dir} && #{rm_tmp_cmd}"
+      system "#{rm_tmp_cmd}; git clone #{url} #{dir}/tmp && mv #{dir}/tmp/* #{dir} && #{rm_tmp_cmd}"
     end
 
     def gitignore_add(pattern = nil)
