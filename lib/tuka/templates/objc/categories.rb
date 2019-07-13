ba93c3d76a13e7792c0b6658449c85d3bb235dee
ffd <ffdelacruz@icloud.com>
2019-07-13 14:39:56 +0800

    Privatize some methods in ObjC module; Start filling the functionality of ObjC::Category

--- lib/tuka/templates/objc/categories.rb
+++ lib/tuka/templates/objc/categories.rb
@@ -2,18 +2,18 @@
 
 module Tuka
   module ObjC
-    def category_files(file_type: nil, framework: nil)
-      all = Dir[File.join(__dir__, '**', framework.to_s, "*.#{file_type || '{h,m}'}")]
-      all.group_by { |file| File.basename(file, '.*').itself }.values
-    end
-
     class Category
       DEFAULT_PREFIX = 'MXBZ'
 
+      attr_reader :header, :implementation, :framework, :prefix, :header_content, :implementation_content
+
       def initialize(header:, implementation:)
-        @header = File.basename(header)
-        @implementation = File.basename(implementation)
+        @header = header
+        @implementation = implementation
+        @framework = File.basename(Pathname.new(implementation).parent)
         @prefix = DEFAULT_PREFIX
+        @header_content = File.open(header).read
+        @implementation_content = File.open(implementation).read
       end
 
       def filename
@@ -21,6 +21,7 @@ module Tuka
       end
 
       def prefix=(prefix)
+        raise StandardError, 'Invalid category prefix' unless prefix.match(/^[[:alpha:]]{3,5}$/)
         apply_prefix
         @prefix = prefix
       end
@@ -28,8 +29,20 @@ module Tuka
       private
 
       def apply_prefix(prefix)
-
+        @header_content.gsub!(DEFAULT_PREFIX, prefix)
       end
+    end
+
+    def categories_sample(count = 1)
+      samples = [] + category_files.sample(count)
+      samples.map { |sample| Category.new(header: sample.first, implementation: sample.last) }
+    end
+
+    private
+
+    def category_files(file_type: nil, framework: nil)
+      all = Dir[File.join(__dir__, '**', framework.to_s, "*.#{file_type || '{h,m}'}")]
+      all.group_by { |file| File.basename(file, '.*').itself }.values
     end
   end
 end
