31c425fd1e82026f0a136f9439e0c747d4b824fd
ffd <ffdelacruz@icloud.com>
2019-07-15 12:05:02 +0800

    Delete the ObjectiveC method that can cause compilation errors due to escaping when rendered in the project

--- lib/tuka/templates/objc/categories/foundation/NSString+MXBZContains.m
+++ lib/tuka/templates/objc/categories/foundation/NSString+MXBZContains.m
@@ -25,18 +25,6 @@
     return NO;
 }
 
-- (NSString *)mxbz_makeUnicodeToString
-{
-    NSString *tempStr1 = [self stringByReplacingOccurrencesOfString:@"\\u"withString:@"\\U"];
-    NSString *tempStr2 = [tempStr1 stringByReplacingOccurrencesOfString:@"\""withString:@"\\\""];
-    NSString *tempStr3 = [[@"\""stringByAppendingString:tempStr2] stringByAppendingString:@"\""];
-    NSData *tempData = [tempStr3 dataUsingEncoding:NSUTF8StringEncoding];
-    
-    NSString *returnStr = [NSPropertyListSerialization propertyListWithData:tempData options:NSPropertyListMutableContainersAndLeaves format:NULL error:NULL];
-    
-    return [returnStr stringByReplacingOccurrencesOfString:@"\\r\\n"withString:@"\n"];
-}
-
 - (BOOL)mxbz_containsCharacterSet:(NSCharacterSet *)set
 {
     NSRange rang = [self rangeOfCharacterFromSet:set];
