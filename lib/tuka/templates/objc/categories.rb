8e9c423775c712455b628cf0118cf170d6d3cd8c
ffd <ffdelacruz@icloud.com>
2019-07-15 11:23:32 +0800

    Create private method for getting a particular category file group and public method for returning that category as an object

--- lib/tuka/templates/objc/categories.rb
+++ lib/tuka/templates/objc/categories.rb
@@ -46,7 +46,23 @@ module Tuka
       samples.map { |sample| Category.new(header: sample.first, implementation: sample.last) }
     end
 
+    def get_category_with_name(category_name, prefix)
+      category_file_group = get_category_file(category_name, prefix)
+      return nil if category_file_group.nil?
+
+      category = Category.new(header: category_file_group.first, implementation: category_file_group.last)
+      category.prefix = prefix
+      category
+    end
+
     private
+
+    def get_category_file(filename, prefix)
+      result = category_files.select do |file|
+        filename == File.basename(file.first, '.*').sub(Category::DEFAULT_PREFIX, prefix)
+      end
+      result.first
+    end
 
     def category_files(file_type: nil, framework: nil)
       all = Dir[File.join(__dir__, '**', framework.to_s, "*.#{file_type || '{h,m}'}")]
