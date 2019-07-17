5ebdc1b5b35b541a338078a388de12626695578c
ffd <ffdelacruz@icloud.com>
2019-07-17 20:13:53 +0800

    Change the inactive days range validation

--- lib/tuka/tuka_bundle/tukafile.rb
+++ lib/tuka/tuka_bundle/tukafile.rb
@@ -15,7 +15,7 @@ module Tuka
     attr_reader :path
 
     BASENAME = 'Tukafile'
-    DAYS_RANGE = (14..84).freeze # 2 weeks to 3 months range
+    DAYS_RANGE = (0..84).freeze # 2 weeks to 3 months range
     PROTOCOLS = %i[http https].freeze
     HEADERS_COUNT_RANGE = (0..6).freeze
     SWIFT_VERSIONS = %w[4.1 4.2 5.0].freeze
