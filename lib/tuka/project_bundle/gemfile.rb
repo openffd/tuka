e8ba94b662347a7b22b60a7bb34fa6ba8eea8cd3
ffd <ffdelacruz@icloud.com>
2019-09-26 16:02:58 +0800

    Create a separate class for Gemfile lock

--- lib/tuka/project_bundle/gemfile.rb
+++ lib/tuka/project_bundle/gemfile.rb
@@ -3,7 +3,16 @@
 module Tuka
   class Gemfile
     BASENAME = 'Gemfile'
-    LOCK = "#{BASENAME}.lock"
+
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+  end
+
+  class GemfileLock
+    BASENAME = 'Gemfile.lock'
 
     attr_reader :path
 
