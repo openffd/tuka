77e416f9f69883d6863b0b2e7c31b86e65e989c5
ffd <ffdelacruz@icloud.com>
2019-10-01 15:20:30 +0800

    Create PrlctlCmdNotFoundError class; Add #ls_running_vms

--- lib/tuka/prlctl.rb
+++ lib/tuka/prlctl.rb
@@ -3,5 +3,23 @@
 module Tuka
   module Prlctl
     COMMAND = 'prlctl'
+
+    class PrlctlCmdNotFoundError < StandardError
+      def initialize(msg = 'Command not found: ' + COMMAND)
+        super(msg)
+      end
+    end
+
+    def ls_running_vms
+      raise PrlctlCmdNotFoundError if cmd_not_found?
+
+      `#{COMMAND} list -j`
+    end
+
+    private
+
+    def cmd_not_found?
+      system(COMMAND).nil?
+    end
   end
 end
