cdf1e8aa025a9a4ecb701981cc00383496b3d1d4
ffd <ffdelacruz@icloud.com>
2019-10-14 18:53:59 +0800

    Create entitlements.rb file

---
+++ lib/tuka/project_bundle/entitlements.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Tuka
+  require 'cfpropertylist'
+
+  class Entitlements
+    attr_reader :path
+
+    APS_ENVIRONMENT = 'aps-environment'
+    APS_ENVIRONMENT_VALUE = 'development'
+
+    def initialize(path)
+      @path = path
+      @plist = CFPropertyList::List.new(file: @path)
+    end
+
+    def aps_environment_setup?
+      @plist.value.value[APS_ENVIRONMENT].to_s.eql? APS_ENVIRONMENT_VALUE
+    end
+  end
+end
