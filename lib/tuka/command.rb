84a9391de6dee82f03529e45e347867a485fea15
ffd <ffdelacruz@icloud.com>
2019-10-15 14:12:40 +0800

    Fix condition checking whether entitlements are properly setup

--- lib/tuka/command.rb
+++ lib/tuka/command.rb
@@ -48,7 +48,8 @@ module Tuka
 
     def check_push_notification_capabilities
       message = 'Push notifications not enabled for this project, please go to Capabilities and check the entitlements'
-      raise StandardError, message unless project.push_notifications_enabled? || entitlements.aps_environment_setup?
+      aps_environment_setup = entitlements && entitlements.aps_environment_setup?
+      raise StandardError, message unless project.push_notifications_enabled? || aps_environment_setup
     end
 
     def kill_all_vm_xcode
